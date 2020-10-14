9c031262b5196584e0add33c2e074d5e5c918cd3
OrianCohen <orianc561@gmail.com>
2020-10-14 17:35:42 +0300

    Added requirements file

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+from random import random
+
 from flask import Flask, jsonify, request, abort, make_response
-from flask_restful import Api
-from flask_httpauth import HTTPBasicAuth
+from flask_restful import Resource, Api, reqparse
+from datetime import datetime
+import requests
+
 app = Flask(__name__)
 api = Api(app)
-# http://127.0.0.1:5000/message/api/v1.0/messages
-# Authentication access for existing user
-auth = HTTPBasicAuth()
 
 MESSAGEJSON = [
     {
@@ -14,7 +15,9 @@ MESSAGEJSON = [
         "receiver": "support@apple.com",
         "message": "I would like to know if you working this week? I want to purchase the Iphon 11 PRO",
         "subject": "Purchase new IPHONE",
-        "creationDate": "datetime.timestamp(datetime.now())"
+        "creationDate": "datetime.timestamp(datetime.now())",
+        "readStatus": True
+
     },
     {
         "id": 2,
@@ -22,80 +25,101 @@ MESSAGEJSON = [
         "receiver": "Bank of America",
         "message": "I would like to open a new bank account",
         "subject": "New account",
-        "creationDate": "datetime.timestamp(datetime.now())"
+        "creationDate": "datetime.timestamp(datetime.now())",
+        "readStatus": True
+    },
+    {
+        "id": 3,
+        "sender": "Loren",
+        "receiver": "Orian",
+        "message": "XXXXX",
+        "subject": "New account",
+        "creationDate": "datetime.timestamp(datetime.now())",
+        "readStatus": False
+    },
+    {
+        "id": 4,
+        "sender": "Loren",
+        "receiver": "Coral",
+        "message": "XXXXX",
+        "subject": "New account",
+        "creationDate": "datetime.timestamp(datetime.now())",
+        "readStatus": False
     }
-
 ]
 
-# @auth.get_password
-# def get_password(username):
-#     if username == 'Orian':
-#         return 'Hu432!'
-#     return None
+message_put_args = reqparse.RequestParser()
+message_put_args.add_argument("sender", type=str)
+message_put_args.add_argument("receiver", type=str)
+message_put_args.add_argument("message", type=str)
+message_put_args.add_argument("subject", type=str)
+
+#
+# def abort_if_user_exist(user_name):
+#     abort(409, message="")
 #
 #
-# @auth.error_handler
-# def unauthorized():
-#     return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+# def abort_if_user_name_doesnt_exist(user_name):
+#     for item in MESSAGEJSON:
+#         if user_name not in item:
+#             abort(409, message="User nor exist")
 
 
-# If we tried to invoke this function  with curl the access wil be denied, to invoke we need to send out credentials
-# $ curl -u Orian:Hu432 -i http://localhost:5000/message/api/v1.0/messages
-# @app.route('/message/api/v1.0/messages', methods=['GET'])
-# @auth.login_required
-# def get_tasks():
-#     return jsonify({'Messages': MESSAGEJSON})
+class Message(Resource):
+    # read a random messages
+    @staticmethod
+    def get():
+        return MESSAGEJSON[0]
 
 
-# If we will ask for message that not exist in our data
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify(({'error': 'Not found'}), 404))
+class ReadMessage(Resource):
+    # get all the messages from sender or receiver (for specific name)
+    def get(self, user_name):
+        message = [[message for message in MESSAGEJSON if
+                    (message['sender'].lower() == user_name.lower() or message[
+                        'receiver'].lower() == user_name.lower())]]
+        if len(message) == 0:
+            abort(404)
+        return jsonify({'Message by user name': message[0]})
+
+    # add new message to our data
+    def put(self, user_name):
+        new_message = message_put_args.parse_args()
+        new_message['id'] = MESSAGEJSON[-1]['id'] + 1
+        new_message['creationDate'] = str(datetime.now())
+        new_message['readStatus'] = False
+        MESSAGEJSON.append(new_message)
+        return MESSAGEJSON[new_message['id'] -1]
+
+    # delete message as receiver or sender (for specific name)
+    def delete(self, user_name):
+        # abort_if_user_name_doesnt_exist(user_name)
+        message = [message for message in MESSAGEJSON if
+                   (message['sender'].lower() == user_name.lower() or message['receiver'].lower() == user_name.lower())]
+        if len(message) == 0:
+            abort(404)
+        MESSAGEJSON.remove(message[0])
+        return jsonify({'Result': True})
+
+# get all unread messages by name
+class UnreadMessage(Resource):
+    def get(self, user_name):
+        message = [[message for message in MESSAGEJSON if
+                    (message['receiver'].lower() == user_name.lower()) and (
+                                message['readStatus'] == False)]]
+        if len(message) == 0:
+            abort(404)
+        return jsonify({'Unread Messages': message[0]})
 
 
-# Reading all messages in our DATA
-@app.route('/message/api/v1.0/messages', methods=['GET'])
-def get_messages():
-    # return jsonify({'All Messages': AllMessage.get()})
-    return jsonify({'All Messages': MESSAGEJSON})
+# return all messages (it can be only 1 message as well)
+api.add_resource(Message, '/messages')
 
+# return all messages for specific user + delete message + add new message
+api.add_resource(ReadMessage, '/messages/read/<user_name>')
 
-# Read message by ID number
-@app.route('/message/api/v1.0/messages/<int:messageId>', methods=['GET'])
-def get_message_by_id(messageId):
-    message = [message for message in MESSAGEJSON if message['id'] == messageId]
-    if len(message) == 0:
-        abort(404)
-    return jsonify({'Message by ID': message[0]})
+# return all unread messages for specific user
+api.add_resource(UnreadMessage, '/messages/unread/<user_name>')
 
-
-# Delete message by name
-@app.route('/message/api/v1.0/messages/<name>', methods=['DELETE'])
-def delete_message(name):
-    message = [message for message in MESSAGEJSON if (message['sender'] == name or message['receiver'] == name)]
-    if len(message) == 0:
-        abort(404)
-    MESSAGEJSON.remove(message[0])
-    return jsonify({'Result': True})
-
-
-# curl -i -H "Content-Type: application/json" -X POST -d http://localhost:5000/message/api/v1.0/messages
-# Write a new message
-@app.route('/message/api/v1.0/messages', methods=['POST'])
-def new_message():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    create_message = {
-        'id': MESSAGEJSON[-1]['id'] + 1,
-        'sender': request.json.get('sender', ""),
-        'receiver': request.json.get('receiver', ""),
-        'message': request.json.get('message', ""),
-        'subject': request.json.get('subject', ""),
-        'creationDate': request.json.get('creationDate', "")
-    }
-    MESSAGEJSON.append(create_message)
-    return jsonify({'New Message': create_message}), 201
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
